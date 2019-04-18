bc2c46116d25012552aa5608b03ee91f9e691f5a
Klaas Vandeweerdt <klaas_vandeweerdt@hotmail.com>
2019-04-18 02:06:19 +0200

    Updated dev branch

--- Hello World/HelloWorldCmd/Program.cs
+++ Hello World/HelloWorldCmd/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorldCmd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world 2");
             Console.ReadKey();
         }
     }
